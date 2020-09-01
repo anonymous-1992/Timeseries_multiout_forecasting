41716820f2ea442117a505990fd8993eab3381ce
sepidehkoohfar <33998911+sepidehkoohfar@users.noreply.github.com>
2020-09-01 13:17:28 -0400

    Create run.sh
    Adding bash file to run all the methods

---
+++ run.sh
@@ -0,0 +1,15 @@
+python multiout_forecast_rnn.py --name LSTM
+python multiout_forecast_rnn.py --name BiLSTM
+python multiout_forecast_rnn.py --name EdLSTM
+python multiout_forecast_rnn.py --name BiEdLSTM
+python multiout_forecast_rnn.py --name CNN
+python multiout_forecast_rnn.py --name GRU
+python multiout_forecast_rnn.py --name BiGRU
+
+python multiout_forecast_reg.py --name LR
+python multiout_forecast_reg.py --name SVR
+python multiout_forecast_reg.py --name Lasso
+python multiout_forecast_reg.py --name GP
+
+python multiout_forecast_stats.py --name ARIMA
+python multiout_forecast_stats.py --name VAR
