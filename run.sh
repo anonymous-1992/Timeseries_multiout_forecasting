0d083f9dfd619acf7ed0ab383abd8c9f308404ed
sepidekoohfar <sepide1.koohfar@gmail.com>
2020-09-06 21:39:02 -0400

    change horizon to 8

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
-python lib/multiout_forecast_rnn.py --name LSTM --save results.txt
-python lib/multiout_forecast_rnn.py --name BiLSTM --save results.txt
-python lib/multiout_forecast_rnn.py --name EdLSTM --save results.txt
-python lib/multiout_forecast_rnn.py --name BiEdLSTM --save results.txt
-python lib/multiout_forecast_rnn.py --name CNN --save results.txt
-python lib/multiout_forecast_rnn.py --name GRU --save results.txt
-python lib/multiout_forecast_rnn.py --name BiGRU --save results.txt
+python lib/multiout_forecast_rnn.py --name LSTM --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name BiLSTM --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name EdLSTM --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name BiEdLSTM --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name CNN --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name GRU --save results.txt --horizon 8
+python lib/multiout_forecast_rnn.py --name BiGRU --save results.txt --horizon 8
 
-python lib/multiout_forecast_reg.py --name LR --save results.txt
-python lib/multiout_forecast_reg.py --name SVR --save results.txt
-python lib/multiout_forecast_reg.py --name Lasso --save results.txt
-python lib/multiout_forecast_reg.py --name GP --save results.txt
+python lib/multiout_forecast_reg.py --name LR --save results.txt --horizon 8
+python lib/multiout_forecast_reg.py --name SVR --save results.txt --horizon 8
+python lib/multiout_forecast_reg.py --name Lasso --save results.txt --horizon 8
+python lib/multiout_forecast_reg.py --name GP --save results.txt --horizon 8
 
-python lib/multiout_forecast_com.py --save results.txt
+python lib/multiout_forecast_com.py --save results.txt --horizon 8
 
 
